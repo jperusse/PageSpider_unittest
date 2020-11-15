4aa260d4e154b913d05b3f415ea64cbdd004e1bb
Jim Perusse <67986763+jperusse@users.noreply.github.com>
2020-11-14 18:05:22 -0800

    Reformatted all code

--- page_spider.py
+++ page_spider.py
@@ -6,10 +6,11 @@ def main(database: str, url_file_list: str):
     print("We are going to work with " + database)
     print("We are going to scan " + url_file_list)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-db","--database",help="SQLite File Name")
-    parser.add_argument("-i","--input",help="File containing urls to read")
+    parser.add_argument("-db", "--database", help="SQLite File Name")
+    parser.add_argument("-i", "--input", help="File containing urls to read")
     args = parser.parse_args()
     database_file = args.database
     input_file = args.input
--- test/test_url_utilities.py
+++ test/test_url_utilities.py
@@ -3,6 +3,7 @@ from utilities.url_utilities import load_urls_from_file
 from utilities.url_utilities import load_page
 from utilities.url_utilities import scrape_page
 
+
 class TestURLUtils(unittest.TestCase):
     def test_read_file_with_urls(self):
         urls = load_urls_from_file("urls.txt")
@@ -14,6 +15,7 @@ class TestURLUtils(unittest.TestCase):
 
     def test_scrape_page(self):
         results = scrape_page(page_contents="https//:www.google.com")
+        self.assertTrue(results)
 
 
 if __name__ == '__main__':
