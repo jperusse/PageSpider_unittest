3dd90ba24ba7ed165b65bd149fe150580d264f08
Jim Perusse <67986763+jperusse@users.noreply.github.com>
2020-11-16 10:48:13 -0800

    more updates

--- test/test_database_utilities.py
+++ test/test_database_utilities.py
@@ -5,7 +5,7 @@ from utilities.database_utilities import save_words_to_database
 
 class TestDBUtils(unittest.TestCase):
     def test_read_file_with_urls(self):
-        results = create_database("db_name")
+        results = create_database("word.db")
         self.assertTrue(results)
 
     def test_save_words_to_database(self):
