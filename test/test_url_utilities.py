de40c03a40440955325e3b497ceada59f9a04646
Jim Perusse <67986763+jperusse@users.noreply.github.com>
2020-11-17 14:15:07 -0800

    added scrape_page code

--- test/test_url_utilities.py
+++ test/test_url_utilities.py
@@ -3,13 +3,14 @@ from utilities.url_utilities import load_urls_from_file
 from utilities.url_utilities import load_page
 from utilities.url_utilities import scrape_page
 
-def setup():
-    print("setup run")
-
-def teardown():
-    print("teardown run")
 
 class TestURLUtils(unittest.TestCase):
+    def setUp(self):
+        self.url1 = "https//:www.google.com"
+
+    def tearDown(self):
+        pass
+
     def test_read_file_with_urls_missing_file(self):
         urls = load_urls_from_file("missing_file.txt")
         self.assertEqual(len(urls), 0)
@@ -27,13 +28,12 @@ class TestURLUtils(unittest.TestCase):
         self.assertGreaterEqual(len(urls), 1)
 
     def test_load_page(self):
-        url1 = "https//:www.google.com"
-        page = load_page(url1)
-        self.assertTrue(page)
+        html = load_page(self.url1)
+        self.assertTrue(html)
 
     def test_scrape_page(self):
-        url1 = "https//:www.google.com"
-        results = scrape_page(page_contents=url1)
+        html = load_page(self.url1)
+        results = scrape_page(page_contents=html)
         self.assertTrue(results)
 
 
--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,18 +1,65 @@
+import re
+import string
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+
 def load_urls_from_file(file_path: str):
     url = []
     try:
         with open(file_path) as f:
             url.append(f.readline())
             return url
-    except:
+    except FileNotFoundError:
         return url
 
 
 def load_page(url: str):
-    # TODO:  add the code that reads the url
-    return True
+    response = urlopen(url)
+    html = response.read().decode("utf-8")
+    return html
 
 
 def scrape_page(page_contents: str):
-    # TODO:  analyze the text
-    return True
+    chicken_noodle = BeautifulSoup(page_contents, "html5lib")
+
+    for script in chicken_noodle(["script", "style"]):
+        script.extract()
+
+    text = chicken_noodle.get_text()
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+
+    text = ' '.join(chunk for chunk in chunks if chunk)
+    plain_text = ''.join(filter(lambda x: x in string.printable, text))
+
+    clean_words = []
+
+    words = plain_text.split(" ")
+    for word in words:
+        clean = True
+
+        # no punctuation
+        for punctuation_marks in string.punctuation:
+            if punctuation_marks in word:
+                clean = False
+
+                # no numbers
+            if any(char.isdigit() for char in word):
+                clean = False
+
+                # at least two characters but no more than 10
+            if len(word) < 2 or len(word) > 10:
+                clean = False
+
+            if not re.match(r'^\w+$', word):
+                clean = False
+
+            if clean:
+                try:
+                    clean_words.append(word.lower())
+                except UnicodeEncodeError:
+                    print(".")
+
+    return clean_words
